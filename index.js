9addab1566085972f0b35070eae4d581c0278690
Josue <saquirayjosue@gmail.com>
2025-06-02 23:26:31 -0500

    Avance 2

--- index.js
+++ index.js
@@ -37,35 +37,74 @@ function shuffle(array) {
 
 io.on('connection', (socket) => {
   socket.on('create_room', ({ name, room }) => {
+    socket.data.name = name;
     socket.join(room);
     rooms[room] = rooms[room] || [];
     rooms[room].push(name);
     io.to(room).emit('room_update', { players: rooms[room] });
+    console.log(`Sala creada: ${room} por ${name}`);
   });
 
   socket.on('join_room', ({ name, room }) => {
+    socket.data.name = name;
     socket.join(room);
     rooms[room] = rooms[room] || [];
     rooms[room].push(name);
     io.to(room).emit('room_update', { players: rooms[room] });
+    console.log(`${name} se unió a la sala: ${room}`);
   });
 
   socket.on('start_game', ({ room }) => {
   const deck = shuffle(generateDeck());
   const playersInRoom = rooms[room];
+  console.log(`Iniciando juego en la sala: ${room} con jugadores: ${playersInRoom.join(', ')}`);
+  playersInRoom.forEach((playerName) => {
+  console.log(`Repartiendo cartas a ${playerName} en la sala ${room}`);
+  // Encuentra el socket del jugador en la sala
+  const playerSocket = [...io.sockets.sockets.values()].find(s =>
+    s.rooms.has(room) && s.data.name === playerName
+  );
+  console.log(`Socket del jugador ${playerName}:`, playerSocket ? playerSocket.id : 'No encontrado');
+  // Reparte 10 cartas a cada jugador
+  const hand = deck.splice(0, 10);
 
-  playersInRoom.forEach((playerName, index) => {
-    const playerSocket = [...io.sockets.sockets.values()].find(s =>
-      s.rooms.has(room) && s.handshake.query.name === playerName
-    );
-
-    const hand = deck.splice(0, 10); // reparte 10 cartas a cada uno
-
-    if (playerSocket) {
-      playerSocket.emit('start_game', { hand });
-    }
+  roomState[room] = {
+  turnIndex: 0,
+  deck,
+  players: playersInRoom,
+  hands: {
+    [playerName]: hand,
+    //...
+  }
+};
+  if (playerSocket) {
+    playerSocket.emit('start_game', {
+      hand,
+      turnPlayer: playersInRoom[0]
+});
+  }
+  
   });
   });
+
+  socket.on('play_card', ({ room, card }) => {
+  const state = roomState[room];
+  const playerName = socket.data.name;
+  const hand = state.hands[playerName];
+
+  // eliminar carta de la mano
+  const index = hand.indexOf(card);
+  if (index > -1) {
+    hand.splice(index, 1);
+  }
+
+  // actualizar turno
+  state.turnIndex = (state.turnIndex + 1) % state.players.length;
+  const nextPlayer = state.players[state.turnIndex];
+
+  io.to(room).emit('turn_update', { nextPlayer });
+  });
+
 
 });
 
